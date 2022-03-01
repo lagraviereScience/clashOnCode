86f6c0e62f355830a9bd6fcec38be2e311ad21f8
Jeremie Lagraviere <jeremie.lagraviere@gmail.com>
2022-03-01 19:14:12 +0100

    clash reverse last digit of an integer c# csharp

---
+++ clashReverse/lastDigitOfAnInteger.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+Console.WriteLine(Console.ReadLine()[^1]);
+    }
+}
